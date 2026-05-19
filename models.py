56443e2458e4735351a8ae7faf3df3ac296b27fa
ficserbiYY <ficserbiy@gmail.com>
2026-05-19 14:21:18 +0500

    refactor: models are now in models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from datetime import datetime
+
+
+# Define the Database Model
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
+    hashed_password: str
+    
+class UserCreate(SQLModel):
+    username: str
+    password: str # Plain password
+    
+    
+class Post(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    author: str
+    title: str
+    content: str
+    date_posted: str = Field(default_factory=lambda: datetime.utcnow().strftime("%B %d, %Y"))
